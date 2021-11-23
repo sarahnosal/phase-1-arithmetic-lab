f11c7697e2dd029ad6f4bf88a2de14da106d3acb
Sarah Nosal <nosalsm@gmail.com>
2021-11-22 18:17:01 -0600

    commit tree 7c324c9d1088e57f971b1c71cc6bcb61a9ac7ebc

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1= 31;
+let num2= 2;
+let multiply = num1 * num2;
+let random = Math.round(Math.random());
+const num3= 4;
+const num4= 5;
+let mod= num3 % num4;
+let max= Math.max(0, 1, 2, 20);
